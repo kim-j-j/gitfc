f9ae6bea7258b051abd1bb2fbf8620314aa8f58f
5414117@kmyung.onmicrosoft.com <5414117@kmyung.onmicrosoft.com>
2020-10-09 23:15:37 +0900

    asd

--- push/Form1.cs
+++ push/Form1.cs
@@ -19,7 +19,7 @@ namespace push
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox.Text = "push";
+            textBox.Text = "pppp";
         }
     }
 }
