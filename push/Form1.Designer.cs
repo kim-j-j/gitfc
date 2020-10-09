e88d28c08e28412e9901c0a1d0a901a9c02fbe75
5414117@kmyung.onmicrosoft.com <5414117@kmyung.onmicrosoft.com>
2020-10-09 21:38:58 +0900

    a

--- push/Form1.Designer.cs
+++ push/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -39,22 +40,33 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(263, 194);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(100, 21);
+            this.textBox.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox;
     }
 }
 
--- push/Form1.cs
+++ push/Form1.cs
@@ -16,5 +16,10 @@ namespace push
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox.Text = "push";
+        }
     }
 }
